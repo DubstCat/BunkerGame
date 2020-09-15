b578b28b582c069b9519808ec2d002b0c3a0785a
DubstCat <voronets.yar@gmail.com>
2020-09-15 16:45:59 +0300

    Added a background for each color

--- app/src/main/java/com/voronets/bunkergame/CharactsAdapter.kt
+++ app/src/main/java/com/voronets/bunkergame/CharactsAdapter.kt
@@ -1,19 +1,24 @@
 package com.voronets.bunkergame
 
-
-
-import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.characteristics_layout.view.*
-import org.w3c.dom.Attr
+
 
 class CharactAdapter (
-
     private var characts:List<Charact>
 ):RecyclerView.Adapter<CharactAdapter.CharactViewHolder>(){
+    private var backgroundsList = listOf<Int>(
+        R.drawable.bg_text_red,
+        R.drawable.bg_text_orange,
+        R.drawable.bg_text_orange ,
+        R.drawable.bg_text_green,
+        R.drawable.bg_text_cyan,
+        R.drawable.bg_text_blue ,
+        R.drawable.bg_text_purple)
+
     inner class CharactViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharactViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.characteristics_layout, parent, false)
@@ -25,14 +30,7 @@ class CharactAdapter (
         holder.itemView.apply {
             tv_charact_name.text = characts[position].Name
             tv_charact_description.text = characts[position].Description
-
-
-
-            /*tv_charact_description.setBackgroundColor(Color.parseColor(arrayListOf<String>
-                (
-                "#D40C00", "#FF5500", "#FFEF00", "#32C12C", "#526EFF", "#7F4FC9", "#00BCD9"
-                ).shuffled()[0]))*/
-            R.drawable.bg_characts_text
+            tv_charact_description.background = resources.getDrawable(backgroundsList.shuffled()[0])
         }
     }
 
